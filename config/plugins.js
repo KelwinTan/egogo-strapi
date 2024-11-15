47fb9f68bb99cb457f8644249db03f5c6a15f375
kelwingravhl <kelwin.tantono@gravhl.com>
2024-11-16 01:32:31 +0700

    fix: retry email

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,19 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.ethereal.email"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+      },
+      settings: {
+        defaultFrom: "salma.crona78@ethereal.email",
+        defaultReplyTo: "lw.kelwin@gmail.com",
+      },
+    },
+  },
+});
