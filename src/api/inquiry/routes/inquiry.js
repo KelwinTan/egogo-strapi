e2abeec9abde96e431f54bf71aaaedbba450899d
kelwintan <lw.kelwin@gmail.com>
2024-11-16 02:03:40 +0700

    fix: remove previous invoke test

--- src/api/inquiry/routes/inquiry.js
+++ src/api/inquiry/routes/inquiry.js
@@ -6,18 +6,4 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = {
-  ...createCoreRouter("api::inquiry.inquiry"),
-
-  routes: [
-    {
-      method: "POST", // HTTP method for this route
-      path: "/send-email", // Define the path for the custom route
-      handler: "inquiry.sendEmail", // Reference to the sendEmail method in your controller
-      config: {
-        auth: false, // Optional: Set to true if you need authentication for this route
-      },
-    },
-    // You can add more custom routes here
-  ],
-};
+module.exports = createCoreRouter("api::inquiry.inquiry");
