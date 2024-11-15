9b1ee602281aec5eed3f4b7f74272ec7652c6f60
kelwintan <lw.kelwin@gmail.com>
2024-11-16 02:00:31 +0700

    feat: test send-email invoke

--- src/api/inquiry/routes/inquiry.js
+++ src/api/inquiry/routes/inquiry.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * inquiry router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::inquiry.inquiry');
+module.exports = {
+  ...createCoreRouter("api::inquiry.inquiry"),
+
+  routes: [
+    {
+      method: "POST", // HTTP method for this route
+      path: "/send-email", // Define the path for the custom route
+      handler: "inquiry.sendEmail", // Reference to the sendEmail method in your controller
+      config: {
+        auth: false, // Optional: Set to true if you need authentication for this route
+      },
+    },
+    // You can add more custom routes here
+  ],
+};
