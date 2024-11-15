0294c79fdf5cbaa11a3c5e3cbc48ffcb8e2b8ae8
kelwingravhl <kelwin.tantono@gravhl.com>
2024-11-16 00:53:46 +0700

    fix: remove smtp for now

--- src/api/inquiry/services/inquiry.js
+++ src/api/inquiry/services/inquiry.js
@@ -6,18 +6,4 @@
 
 const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService("api::inquiry.inquiry", ({ strapi }) => ({
-  async sendEmail() {
-    try {
-      await strapi.plugins["email"].services.email.send({
-        from: "salma.crona78@ethereal.email",
-        to: "lw.kelwin@gmail.com",
-        subject: "Hello ✔",
-        html: `<p><b>Hello</b> world</p>`,
-      });
-      strapi.log.info("Email sent successfully!");
-    } catch (error) {
-      strapi.log.error("Error sending email:", error);
-    }
-  },
-}));
+module.exports = createCoreService("api::inquiry.inquiry");
